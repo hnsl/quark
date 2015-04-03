1251082063c991d1b66d142fa3aca5eb99137f9d
Hannes Landeholm <hannes@jumpstarter.io>
2015-04-03 23:30:52 +0200

    split vm into own file

---
+++ src/quark-internal.h
@@ -0,0 +1,95 @@
+#include "rcd.h"
+#include "musl.h"
+#include "acid.h"
+#include "quark.h"
+
+#pragma librcd
+
+#define QK_HEADER_MAGIC 0x6aef91b6b454b73f
+#define QK_VERSION 2
+
+/// Smallest possible allocation size: 12 2e = 2^12 = 4kib.
+#define QK_PAGE_SIZE_2E 12
+
+/// The default "untuned" target items per partition.
+#define QK_DEFAULT_TARGET_IPP (20)
+
+/// Sanity checks state.
+#define QK_SANTIY_CHECK(x) qk_santiy_check((x), __FILE__, __LINE__)
+
+typedef struct qk_idx {
+    uint16_t keylen;
+    uint8_t* keyptr;
+} __attribute__((packed, aligned(1))) qk_idx_t;
+
+/// B-Skip-List partition header.
+/// [ header, sorted index space --> , gap, <-- data space ]
+/// index structure: {
+///     uint16_t keylen; (length of key)
+///     uint8_t* keyptr; (offset to key from partition start)
+/// }
+/// level0 data structure: {
+///     uint8_t key[]; (key, length found in index)
+///     uint64_t valuelen;
+///     uint8_t value[];
+/// }
+/// level1+ data structure: {
+///     uint8_t key[]; (key, length found in index)
+///     qk_part_t* down;
+/// }
+typedef struct qk_part {
+    // Total size of the partition in bytes.
+    uint64_t total_size;
+    // Number of keys in partition.
+    uint32_t n_keys;
+    // Size of end value segments in bytes in partition.
+    uint64_t data_size;
+} __attribute__((packed, aligned(1))) qk_part_t;
+
+/// Global quark header.
+typedef struct qk_hdr {
+    /// Magic number. Used to verify if the db is initialized.
+    uint64_t magic;
+    /// Version of the db. Should be 1.
+    uint64_t version;
+    /// Session of the db. Initialized with 1 and incremented on every open.
+    uint64_t session;
+    /// TODO: When this is non-zero, don't use variable key length. Should be 0 for now.
+    uint64_t static_key_size;
+    /// Tuning statistics: Total number of items in index.
+    uint64_t total_count;
+    /// Tuning statistics: Total size used by all items in index, including overhead in level 0.
+    uint64_t total_level0_use;
+    /// Tuning parameter: the target items per partition.
+    /// Can be set freely on open if requested.
+    uint16_t target_ipp;
+    /// Memory allocator free list. The smallest is 4k and gets
+    /// twice as large for each size class.
+    void* free_list[32];
+    /// End free list size class. The first free list class that is larger than what has
+    /// ever been allocated. The free list vector is undefined at and above this point.
+    uint8_t free_end_class;
+    /// B-Skip-List root, an entry pointer for each level.
+    qk_part_t* root[8];
+} qk_hdr_t;
+
+CASSERT(sizeof(qk_hdr_t) <= PAGE_SIZE);
+
+struct qk_ctx {
+    acid_h* ah;
+    qk_hdr_t* hdr;
+};
+
+static noret void qk_throw_sanity_error(fstr_t file, int64_t line) {
+    throw(concs("quark detected fatal memory corruption or algorithm error at ", file, ":", line), exception_fatal);
+}
+
+static inline void qk_santiy_check(bool check, fstr_t file, int64_t line) {
+    if (!check) {
+        qk_throw_sanity_error(file, line);
+    }
+}
+
+uint8_t qk_value_to_2e(uint64_t value, bool round_up);
+void* qk_vm_alloc(qk_ctx_t* ctx, uint64_t bytes, uint64_t* out_bytes);
+void qk_vm_free(qk_ctx_t* ctx, void* ptr, uint64_t bytes);
---
+++ src/quark-vm.c
@@ -0,0 +1,96 @@
+/* Copyright © 2014, Jumpstarter AB.
+ * Quark memory allocator. */
+
+#include "quark-internal.h"
+
+/// Returns log2(x) rounded down to the nearest integer.
+static uint8_t qk_log2(uint64_t value) {
+    // First round down to one less than a power of 2.
+    value |= value >> 1;
+    value |= value >> 2;
+    value |= value >> 4;
+    value |= value >> 8;
+    value |= value >> 16;
+    value |= value >> 32;
+    // Lookup in table.
+    const uint8_t de_bruijn_table[64] = {
+        63,  0, 58,  1, 59, 47, 53,  2,
+        60, 39, 48, 27, 54, 33, 42,  3,
+        61, 51, 37, 40, 49, 18, 28, 20,
+        55, 30, 34, 11, 43, 14, 22,  4,
+        62, 57, 46, 52, 38, 26, 32, 41,
+        50, 36, 17, 19, 29, 10, 13, 21,
+        56, 45, 25, 31, 35, 16,  9, 12,
+        44, 24, 15,  8, 23,  7,  6,  5
+    };
+    return de_bruijn_table[((uint64_t)((value - (value >> 1)) * 0x07EDD5E59A4E28C2UL)) >> 58];
+}
+
+uint8_t qk_value_to_2e(uint64_t value, bool round_up) {
+    if ((value & ~1UL) == 0)
+        return 0;
+    return qk_log2(round_up? (value - 1) << 1: value);
+}
+
+static inline uint8_t qk_bytes_to_pages_2e(size_t bytes, bool round_up) {
+    if (bytes <= (1UL << QK_PAGE_SIZE_2E))
+        return 0;
+    return qk_value_to_2e(bytes, round_up) - QK_PAGE_SIZE_2E;
+}
+
+static inline size_t qk_pages_2e_to_bytes(uint8_t pages_2e) {
+    return (1UL << (pages_2e + QK_PAGE_SIZE_2E));
+}
+
+/// Free memory of the specified size class.
+static void qk_vm_push(qk_ctx_t* ctx, void* block, uint8_t pages_2e) {
+    qk_hdr_t* hdr = ctx->hdr;
+    assert(pages_2e < hdr->free_end_class);
+    *((void**) block) = hdr->free_list[pages_2e];
+    hdr->free_list[pages_2e] = block;
+}
+
+/// Allocate memory of specified size class.
+static void* qk_vm_pop(qk_ctx_t* ctx, uint8_t pages_2e) {
+    qk_hdr_t* hdr = ctx->hdr;
+    for (uint8_t i_2e = pages_2e;; i_2e++) {
+        void* block;
+        size_t block_len;
+        if (i_2e >= hdr->free_end_class) {
+            // Out of memory, need to reserve more.
+            hdr->free_end_class = i_2e + 1;
+            block_len = qk_pages_2e_to_bytes(i_2e);
+            fstr_t amem = acid_memory(ctx->ah);
+            acid_expand(ctx->ah, amem.len + block_len);
+            block = amem.str + amem.len;
+            goto has_rblock;
+        }
+        block = hdr->free_list[i_2e];
+        if (block != 0) {
+            // Pop block from free list.
+            hdr->free_list[i_2e] = *((void**) block);
+            // Split block and reinsert until we reach the required size.
+            block_len = qk_pages_2e_to_bytes(i_2e);
+            has_rblock:;
+            for (; i_2e > pages_2e; i_2e--) {
+                *((void**) block) = hdr->free_list[i_2e];
+                hdr->free_list[i_2e] = block;
+                block_len /= 2;
+                block += block_len;
+            }
+            return block;
+        }
+    }
+}
+
+void* qk_vm_alloc(qk_ctx_t* ctx, uint64_t bytes, uint64_t* out_bytes) {
+    uint8_t pages_2e = qk_bytes_to_pages_2e(bytes, true);
+    if (out_bytes != 0)
+        *out_bytes = qk_pages_2e_to_bytes(pages_2e);
+    return qk_vm_pop(ctx, pages_2e);
+}
+
+void qk_vm_free(qk_ctx_t* ctx, void* ptr, uint64_t bytes) {
+    uint8_t pages_2e = qk_bytes_to_pages_2e(bytes, true);
+    qk_vm_push(ctx, ptr, pages_2e);
+}
