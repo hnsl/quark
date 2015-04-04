9495b5e4034105abf369b8f8e2125a695b3b483b
Hannes Landeholm <hannes@jumpstarter.io>
2015-04-04 05:59:18 +0200

    test/vis improvments

--- tests/quark-vis.c
+++ tests/quark-vis.c
@@ -2,27 +2,36 @@
 
 #pragma librcd
 
+dict(uint8_t);
+
 static json_value_t objid(qk_ctx_t* ctx, void* ptr, fstr_t type) {
     size_t n = ((size_t) ptr - (size_t) acid_memory(ctx->ah).str) >> QK_PAGE_SIZE_2E;
     return jstr(sconc(type, "/", fss(fstr_from_uint(n, 16))));
 }
 
-static void qk_vis_part(qk_ctx_t* ctx, uint8_t level, qk_part_t* part, json_value_t nodes, json_value_t edges, json_value_t from_id) {
+static void qk_vis_part(
+    qk_ctx_t* ctx, uint8_t level, qk_part_t* part,
+    json_value_t nodes, json_value_t edges,
+    json_value_t from_id, dict(uint8_t)* visited
+) {
+    qk_hdr_t* hdr = ctx->hdr;
     json_value_t part_id = objid(ctx, part, "part");
-    json_append(nodes, jobj_new(
-        {"group", jstr("partition")},
-        {"id", part_id},
-        {"label", part_id},
-        {"level", jnum(level)},
-    ));
     json_append(edges, jobj_new(
         {"from", from_id},
         {"to", part_id},
     ));
+    if (!dict_insert(visited, uint8_t, jstrv(part_id), 1))
+        return;
+    json_append(nodes, jobj_new(
+        {"group", jstr("partition")},
+        {"id", part_id},
+        {"label", jstr(concs("level #", level, "\n", jstrv(part_id)))},
+    ));
     qk_idx_t* idx0 = qk_part_get_idx0(part);
     qk_idx_t* idxE = idx0 + part->n_keys;
+    json_value_t prev_node_id;
     for (qk_idx_t* idxC = idx0; idxC < idxE; idxC++) {
-        json_value_t node_id = objid(ctx, idxC, "node");
+        json_value_t node_id = jstr(concs(jstrv(part_id), "/node#", (idxC - idx0)));
         json_append(edges, jobj_new(
             {"from", part_id},
             {"to", node_id},
@@ -33,44 +42,88 @@ static void qk_vis_part(qk_ctx_t* ctx, uint8_t level, qk_part_t* part, json_valu
                 {"group", jstr("key-node")},
                 {"id", node_id},
                 {"label", jstr(key)},
-                {"level", jnum(level)},
             ));
             qk_part_t* cpart = *qk_idx1_get_down_ptr(idxC);
-            qk_vis_part(ctx, level - 1, cpart, nodes, edges, node_id);
+            qk_vis_part(ctx, level - 1, cpart, nodes, edges, node_id, visited);
         } else {
             fstr_t value = qk_idx0_get_value(idxC);
             json_append(nodes, jobj_new(
                 {"group", jstr("value-node")},
                 {"id", node_id},
                 {"label", jstr(concs(key, "\n", value))},
-                {"level", jnum(0)},
             ));
         }
+        if (idxC > idx0) {
+            json_append(edges, jobj_new(
+                {"from", prev_node_id},
+                {"to", node_id},
+                {"length", jnum(5)}
+            ));
+        }
+        prev_node_id = node_id;
     }
 }
 
-void qk_vis(qk_ctx_t* ctx, rio_t* out_h) { sub_heap {
+fstr_mem_t* qk_vis_dump_graph(qk_ctx_t* ctx) { sub_heap {
     qk_hdr_t* hdr = ctx->hdr;
     json_value_t nodes = jarr_new();
     json_value_t edges = jarr_new();
+    dict(uint8_t)* visited = new_dict(uint8_t);
+    json_append(nodes, jobj_new(
+        {"group", jstr("header")},
+        {"id", jstr("header")},
+        {"label", jstr("header")},
+    ));
+    json_append(nodes, jobj_new(
+        {"group", jstr("header")},
+        {"id", jstr("roots")},
+        {"label", jstr("roots")},
+    ));
+    json_append(edges, jobj_new(
+        {"from", jstr("header")},
+        {"to", jstr("roots")},
+    ));
+    json_value_t prev_root_id;
     for (uint8_t i_lvl = 0; i_lvl < LENGTHOF(hdr->root); i_lvl++) {
         json_value_t root_id = jstr(concs("root#", i_lvl));
         json_append(nodes, jobj_new(
             {"group", jstr("root")},
             {"id", root_id},
-            {"label", jstr(concs("level #", i_lvl, " root"))},
-            {"level", jnum(i_lvl)},
+            {"label", jstr(concs("level #", i_lvl, "\nroot"))},
         ));
-        qk_vis_part(ctx, i_lvl, hdr->root[i_lvl], nodes, edges, root_id);
+        json_append(edges, jobj_new(
+            {"from", jstr("roots")},
+            {"to", root_id},
+        ));
+        qk_vis_part(ctx, i_lvl, hdr->root[i_lvl], nodes, edges, root_id, visited);
+        if (i_lvl > 0) {
+            json_append(edges, jobj_new(
+                {"from", root_id},
+                {"to", prev_root_id},
+            ));
+        }
+        prev_root_id = root_id;
     }
+    json_append(nodes, jobj_new(
+        {"group", jstr("header")},
+        {"id", jstr("free-lists")},
+        {"label", jstr("free lists")},
+    ));
+    json_append(edges, jobj_new(
+        {"from", jstr("header")},
+        {"to", jstr("free-lists")},
+    ));
+    json_value_t prev_list_id;
     for (uint8_t class = 0; class < hdr->free_end_class; class++) {
         json_value_t list_id = jstr(concs("flist#", class));
-        json_value_t level = jnum(LENGTHOF(hdr->root) + class);
         json_append(nodes, jobj_new(
             {"group", jstr("free-list")},
             {"id", list_id},
             {"label", jstr(concs("free list #", class, "\n", (1 << (QK_PAGE_SIZE_2E + class)), "b"))},
-            {"level", level},
+        ));
+        json_append(edges, jobj_new(
+            {"from", jstr("free-lists")},
+            {"to", list_id},
         ));
         void** chunk = hdr->free_list[class];
         json_value_t prev_id = list_id;
@@ -84,16 +137,26 @@ void qk_vis(qk_ctx_t* ctx, rio_t* out_h) { sub_heap {
                 {"group", jstr("free-chunk")},
                 {"id", cur_id},
                 {"label", cur_id},
-                {"level", level},
             ));
             prev_id = cur_id;
             chunk = *chunk;
         }
+        if (class > 0) {
+            json_append(edges, jobj_new(
+                {"from", prev_list_id},
+                {"to", list_id},
+            ));
+        }
+        prev_list_id = list_id;
     }
-    extern const fstr_t vis_tpl;
-    fstr_t html = fss(fstr_replace(vis_tpl, "$DATA", fss(json_stringify_pretty(jobj_new(
+    return escape(json_stringify_pretty(jobj_new(
         {"nodes", nodes},
         {"edges", edges},
-    )))));
+    )));
+}}
+
+void qk_vis_render(qk_ctx_t* ctx, rio_t* out_h, list(fstr_t)* states) { sub_heap {
+    extern const fstr_t vis_tpl;
+    fstr_t html = fss(fstr_replace(vis_tpl, "$DATA", concs("[", fss(fstr_implode(states, ",")), "]")));
     rio_write(out_h, html);
 }}
--- tests/test.c
+++ tests/test.c
@@ -6,23 +6,110 @@
 
 #pragma librcd
 
-static void qk_vis_dump(qk_ctx_t* qk) { sub_heap {
-    void qk_vis(qk_ctx_t* ctx, rio_t* out_h);
-    rio_t* fh = rio_file_open("qk-vis-dump.html", false, true);
-    rio_file_truncate(fh, 0);
-    qk_vis(qk, fh);
+list(fstr_t)* vis_snaps;
+lwt_heap_t* vis_heap;
+
+static void vis_init() {
+    vis_heap = lwt_alloc_heap();
+    switch_heap (vis_heap) {
+        vis_snaps = new_list(fstr_t);
+    }
+}
+
+static void vis_render(qk_ctx_t* qk) {
+    switch_heap(vis_heap) {
+        void qk_vis_render(qk_ctx_t* ctx, rio_t* out_h, list(fstr_t)* states);
+        rio_t* fh = rio_file_open("qk-vis-dump.html", false, true);
+        rio_file_truncate(fh, 0);
+        qk_vis_render(qk, fh, vis_snaps);
+    }
+    lwt_alloc_free(vis_heap);
+    vis_init();
+}
+
+static void vis_snapshot(qk_ctx_t* ctx) { switch_heap(vis_heap) {
+    fstr_mem_t* qk_vis_dump_graph(qk_ctx_t* ctx);
+    list_push_end(vis_snaps, fstr_t, fss(qk_vis_dump_graph(ctx)));
+    DBGFN("taking snapshot #", list_count(vis_snaps, fstr_t));
 }}
 
-static void test0_alloc(fstr_t data_path, fstr_t journal_path) { sub_heap {
+static void test_open_new_qk(qk_ctx_t** out_qk, acid_h** out_ah) {
+    fstr_t data_path = concs("/var/tmp/.librcd-acid-test.", lwt_rdrand64(), ".data");
+    fstr_t journal_path = concs("/var/tmp/.librcd-acid-test.", lwt_rdrand64(), ".jrnl");
     acid_h* ah = acid_open(data_path, journal_path, ACID_ADDR_0, 0);
-    qk_opt_t opt = {0};
+    qk_opt_t opt = {
+        // Allow test to be deterministic.
+        .dtrm_seed = 1,
+        // Very low target ipp for testing.
+        .target_ipp = 4,
+    };
     qk_ctx_t* qk = qk_open(ah, &opt);
-    qk_vis_dump(qk);
+    *out_qk = qk;
+    *out_ah = ah;
+}
+
+static void test0() { sub_heap {
+    qk_ctx_t* qk;
+    acid_h* ah;
+    test_open_new_qk(&qk, &ah);
+    //vis_snapshot(qk);
+    qk_insert(qk, "50", "fifty");
+    qk_insert(qk, "25", "twentyfive");
+    qk_insert(qk, "75", "seventyfive");
+    qk_insert(qk, "30", "thirty");
+    qk_insert(qk, "60", "sixty");
+    qk_insert(qk, "90", "ninety");
+    qk_insert(qk, "70", "seventy");
+    qk_insert(qk, "80", "eighty");
+    qk_insert(qk, "10", "ten");
+    qk_insert(qk, "20", "twenty");
+    //vis_snapshot(qk);
+    qk_insert(qk, "51", "fiftyone");
+    qk_insert(qk, "26", "twentysix");
+    qk_insert(qk, "76", "seventysix");
+    qk_insert(qk, "31", "thirtyone");
+    qk_insert(qk, "61", "sixtyone");
+    qk_insert(qk, "91", "ninetyone");
+    qk_insert(qk, "71", "seventyone");
+    qk_insert(qk, "81", "eightyone");
+    qk_insert(qk, "11", "eleven");
+    qk_insert(qk, "21", "twentyone");
+    //vis_snapshot(qk);
+    //vis_render(qk);
+    acid_fsync(ah);
+    acid_close(ah);
+}}
+
+
+static void test1() { sub_heap {
+    qk_ctx_t* qk;
+    acid_h* ah;
+    test_open_new_qk(&qk, &ah);
+    vis_snapshot(qk);
+    size_t n = 0;
+    extern fstr_t capitals;
+    fstr_t tail = capitals;
+    for (fstr_t row; fstr_iterate_trim(&tail, "\n", &row);) {
+        fstr_t country, capital;
+        if (!fstr_divide(row, ",", &country, &capital))
+            continue;
+        qk_insert(qk, capital, country);
+        n++;
+        if ((n % 40) == 0) {
+            vis_snapshot(qk);
+        }
+    }
+    vis_snapshot(qk);
+    vis_render(qk);
+    acid_fsync(ah);
+    acid_close(ah);
+
 }}
 
 void rcd_main(list(fstr_t)* main_args, list(fstr_t)* main_env) {
-    fstr_t data_path = concs("/var/tmp/.librcd-acid-test.", lwt_rdrand64(), ".data");
-    fstr_t journal_path = concs("/var/tmp/.librcd-acid-test.", lwt_rdrand64(), ".jrnl");
-    test0_alloc(data_path, journal_path);
+    vis_init();
+    test0();
+    test1();
+    rio_debug("tests done");
     exit(0);
 }
