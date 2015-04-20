835acdfd58908514faa33632a785382cef8b64c9
Hannes Landeholm <hannes@jumpstarter.io>
2015-04-20 22:28:50 +0200

    full scan logic tests

--- tests/test.c
+++ tests/test.c
@@ -119,9 +119,10 @@ static void test1() { sub_heap {
     test_open_new_qk(db_path, &qk, &ah, 0);
     //x-dbg/ vis_snapshot(qk);
     //x-dbg/ print_stats(qk);
-    size_t n = 0;
+    size_t total = 0;
     extern fstr_t capitals;
     bool found_andorra = false;
+    rio_debug("test1: standard insert\n");
     for (fstr_t row, tail = capitals; fstr_iterate_trim(&tail, "\n", &row);) {
         fstr_t country, capital;
         if (!fstr_divide(row, ",", &country, &capital))
@@ -130,6 +131,7 @@ static void test1() { sub_heap {
         fstr_t value;
         atest(!qk_get(qk, capital, &value));
         atest(qk_insert(qk, capital, country));
+        total++;
         atest(qk_get(qk, capital, &value));
 
         atest(fstr_equal(value, country));
@@ -146,6 +148,7 @@ static void test1() { sub_heap {
     //x-dbg/ vis_snapshot(qk);
     //x-dbg/ vis_render(qk);
     //x-dbg/ print_stats(qk);
+    rio_debug("test1: standard read\n");
     for (fstr_t row, tail = capitals; fstr_iterate_trim(&tail, "\n", &row);) { sub_heap {
         fstr_t country, capital;
         if (!fstr_divide(row, ",", &country, &capital))
@@ -156,7 +159,243 @@ static void test1() { sub_heap {
         atest(fstr_equal(value, country));
         atest(!qk_get(qk, concs(capital, "\x00"), &value));
     }}
+    // Test scan.
+    rio_debug("test1: scan default\n");
+    fstr_t keys[total];
+    fstr_t values[total];
+    {
+        // A default scan should read out all entries in ascending order.
+        qk_scan_op_t op = {0};
+        bool eof = false;
+        fstr_t g_scan_mem = fss(fstr_alloc(100 * PAGE_SIZE));
+        fstr_t scan_mem = g_scan_mem;
+        size_t scan_n = qk_scan(qk, op, &scan_mem, &eof);
+        atest(scan_n == total);
+        atest(eof);
+        fstr_t key, prev_key, value;
+        for (size_t i = 0; i < total; i++) {
+            atest(qk_band_read(&scan_mem, &key, &value));
+            //x-dbg/ DBGFN("scan [", i, "] [", key, "] => [", value, "]");
+            keys[i] = key;
+            values[i] = value;
+            if (i > 0) {
+                atest(fstr_cmp_lexical(key, prev_key) > 0);
+            }
+            prev_key = key;
+        }
+        atest(!qk_band_read(&scan_mem, &key, &value));
+    }
+    fstr_t g_scan_mem2 = fss(fstr_alloc(100 * PAGE_SIZE));
+    rio_debug("test1: scan reverse\n");
+    {
+        // Reverse/descending scan.
+        qk_scan_op_t op = {
+            .descending = true
+        };
+        bool eof = false;
+        fstr_t scan_mem = g_scan_mem2;
+        size_t scan_n = qk_scan(qk, op, &scan_mem, &eof);
+        atest(scan_n == total);
+        atest(eof);
+        fstr_t key, value;
+        for (size_t i = 0; i < total; i++) {
+            atest(qk_band_read(&scan_mem, &key, &value));
+            size_t j = total - i - 1;
+            //x-dbg/ DBGFN("rscan [", j, "] [", key, "] => [", value, "]");
+            atest(fstr_equal(key, keys[j]));
+            atest(fstr_equal(value, values[j]));
+        }
+        atest(!qk_band_read(&scan_mem, &key, &value));
+    }
+    rio_debug("test1: scan limited\n");
+    {
+        // Reverse/descending limited scan.
+        for (size_t limit = 1; limit < 4; limit++) sub_heap {
+            // First test limit with number.
+            if (limit == 4)
+                limit = total - 1;
+            qk_scan_op_t op = {
+                .descending = true,
+                .limit = limit
+            };
+            fstr_t post_scan_mem;
+            {
+                bool eof = true;
+                fstr_t scan_mem = g_scan_mem2;
+                size_t scan_n = qk_scan(qk, op, &scan_mem, &eof);
+                post_scan_mem = scan_mem;
+                atest(scan_n == limit);
+                atest(!eof);
+                fstr_t key, value;
+                for (size_t i = 0; i < limit; i++) {
+                    atest(qk_band_read(&scan_mem, &key, &value));
+                    size_t j = total - i - 1;
+                    //x-dbg/ DBGFN("rscan [", j, "] [", key, "] => [", value, "]");
+                    atest(fstr_equal(key, keys[j]));
+                    atest(fstr_equal(value, values[j]));
+                }
+                atest(!qk_band_read(&scan_mem, &key, &value));
+            }
+            // Second test limit with short band.
+            {
+                bool eof = true;
+                op.limit = 0;
+                fstr_t scan_mem = fss(fstr_alloc(post_scan_mem.len));
+                size_t scan_n = qk_scan(qk, op, &scan_mem, &eof);
+                atest(scan_n == limit);
+                atest(!eof);
+                fstr_t key, value;
+                for (size_t i = 0; i < limit; i++) {
+                    atest(qk_band_read(&scan_mem, &key, &value));
+                    size_t j = total - i - 1;
+                    //x-dbg/ DBGFN("rscan [", j, "] [", key, "] => [", value, "]");
+                    atest(fstr_equal(key, keys[j]));
+                    atest(fstr_equal(value, values[j]));
+                }
+                atest(!qk_band_read(&scan_mem, &key, &value));
+            }
+        }
+    }
+    rio_debug("test1: scan partials\n");
+    {
+        // Partial scans.
+        for (size_t start = 0; start < total + 1; start++) {
+            for (size_t end = start; end < total + 1; end++) {
+                for (size_t dir = 0; dir < 2; dir++) {
+                    for (size_t inc = 0; inc < 4; inc++) {
+                        for (size_t part = 0; part < 4; part++) sub_heap {
+                            //x-dbg/ DBGFN("partial scan operation start:[", start, "] end:[", end, "] dir:[", dir, "] inc:[", inc, "] part:[", part, "]");
+                            qk_scan_op_t op = {
+                                .key_start = start > 0? keys[start - 1]: "",
+                                .key_end = end > 0? keys[end - 1]: "",
+                                .with_start = (start != 0),
+                                .with_end = (end != 0),
+                                .inc_start = (inc == 1 || inc == 3),
+                                .inc_end = (inc == 2 || inc == 3),
+                            };
+                            bool pm_start = false, pm_end = false;
+                            if (part == 1 || part == 3) {
+                                // Partial matching start key.
+                                if (start == 0)
+                                    continue;
+                                op.key_start = concs(op.key_start, "!");
+                                pm_start = true;
+                            }
+                            if (part == 2 || part == 3) {
+                                // Partial end key.
+                                if (end == 0)
+                                    continue;
+                                op.key_end = concs(op.key_end, "!");
+                                pm_end = true;
+                            }
+                            if (dir == 1) {
+                                op.descending = true;
+                                FLIP(op.key_start, op.key_end);
+                                FLIP(op.with_start, op.with_end);
+                                FLIP(op.inc_start, op.inc_end);
+                                FLIP(pm_start, pm_end);
+                            }
+                            bool eof = ((start % 2) == 0);
+                            fstr_t scan_mem = g_scan_mem2;
+
+                            /*
+                            DBGFN("partial scan operation"
+                                " key_start:[", (op.with_start? op.key_start: "n/a"), "]"
+                                " key_end:[", (op.with_end? op.key_end: "n/a"), "]"
+                                " inc_start:[", (op.inc_start? "yes": "no"), "]"
+                                " inc_end:[", (op.inc_end? "yes": "no"), "]"
+                                " order:[", (op.descending? "desc": "asc"), "]"
+                                " limit:[", (op.limit > 0? STR(op.limit): "∞"), "]"
+                            );
+                            */
+                            size_t scan_n = qk_scan(qk, op, &scan_mem, &eof);
+
+                            ssize_t start_i = start > 0? start - 1: 0;
+                            ssize_t end_i = end > 0? end - 1: total - 1;
+                            size_t read_n = 0;
+
+                            fstr_t key, value;
+                            if (op.descending) {
+                                FLIP(start_i, end_i);
+                                bool expect_eof = (start_i == 0) && (!op.inc_start && !pm_start);
+                                for (ssize_t i = start_i; i >= end_i; i--) {
+                                    if (i == start_i) {
+                                        if (op.with_start && (!op.inc_start && !pm_start))
+                                            continue;
+                                    }
+                                    if (i == end_i) {
+                                        if (op.with_end && (!op.inc_end || pm_end))
+                                            continue;
+                                        expect_eof = !op.with_end;
+                                    }
+                                    // DBGFN("reading band, key #", i);
+                                    if (!qk_band_read(&scan_mem, &key, &value)) {
+                                        rio_debug(concs("unexpected end of scan band on key [", key, "] (#", i, ")\n"));
+                                        atest(false);
+                                    }
+                                    read_n++;
+                                    atest(fstr_equal(key, keys[i]));
+                                    atest(fstr_equal(value, values[i]));
+                                }
+                                if (qk_band_read(&scan_mem, &key, &value)) {
+                                    rio_debug(concs("unexpected continuation of scan band, got key [", key, "] (#", (end_i + 1), ")\n"));
+                                    atest(false);
+                                }
+                                atest(eof == expect_eof);
+                            } else {
+                                bool expect_eof = (start_i >= total - 1) && (!op.inc_start || pm_start);
+                                for (ssize_t i = start_i; i <= end_i; i++) {
+                                    if (i == start_i) {
+                                        if (op.with_start && (!op.inc_start || pm_start))
+                                            continue;
+                                    }
+                                    if (i == end_i) {
+                                        if (op.with_end && (!op.inc_end && !pm_end))
+                                            continue;
+                                        expect_eof = !op.with_end || (i == total - 1 && pm_end);
+                                    }
+                                    // DBGFN("reading band, key #", i);
+                                    if (!qk_band_read(&scan_mem, &key, &value)) {
+                                        rio_debug(concs("unexpected end of scan band on key [", key, "] (#", i, ")\n"));
+                                        atest(false);
+                                    }
+                                    read_n++;
+                                    atest(fstr_equal(key, keys[i]));
+                                    atest(fstr_equal(value, values[i]));
+                                }
+                                if (qk_band_read(&scan_mem, &key, &value)) {
+                                    rio_debug(concs("unexpected continuation of scan band, got key [", key, "] (#", (end_i + 1), ")\n"));
+                                    atest(false);
+                                }
+                                atest(eof == expect_eof);
+                            }
+                            atest(scan_n == read_n);
+                        }
+                    }
+                }
+            }
+        }
+        qk_scan_op_t op = {
+            .descending = true
+        };
+        bool eof = false;
+        fstr_t g_scan_mem = fss(fstr_alloc(100 * PAGE_SIZE));
+        fstr_t scan_mem = g_scan_mem;
+        size_t scan_n = qk_scan(qk, op, &scan_mem, &eof);
+        atest(scan_n == total);
+        atest(eof);
+        fstr_t key, value;
+        for (size_t i = 0; i < total; i++) {
+            atest(qk_band_read(&scan_mem, &key, &value));
+            size_t j = total - i - 1;
+            //x-dbg/ DBGFN("rscan [", j, "] [", key, "] => [", value, "]");
+            atest(fstr_equal(keys[j], key));
+            atest(fstr_equal(values[j], value));
+        }
+        atest(!qk_band_read(&scan_mem, &key, &value));
+    }
     // Test update.
+    rio_debug("test1: update\n");
     {
         size_t i = 0;
         for (fstr_t row, tail = capitals; fstr_iterate_trim(&tail, "\n", &row);) { sub_heap {
